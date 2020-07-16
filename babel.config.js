4efc85cd205d2a1808b8fe436e61ed77fcf189b7
kongxiangyan <kcigaret@outlook.com>
2020-07-16 18:41:42 +0800

    chore: add babel-loader

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [['@babel/preset-env', {
+    targets: {
+      browser: ['> 1%']
+    }
+  }]],
+  plugins: [['@babel/plugin-transform-runtime', {
+    absoluteRuntime: false,
+    corejs: false,
+    helpers: true,
+    regenerator: true,
+    useESModules: false,
+    version: '7.0.0-beta.0'
+  }]]
+}
