e0179d46b380d1b4b14972fea8527be49935b77f
kongxiangyan <kcigaret@outlook.com>
2021-02-02 12:53:24 +0800

    chore: build stuffs

---
+++ build/static.898b51abbc9478e30a05.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdatetemplate"]("static",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "1eba680ff0a7f23e72dd"
+/******/ 	})();
+/******/ 	
+/******/ }
+);
+//# sourceMappingURL=static.898b51abbc9478e30a05.hot-update.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './statics/styles/style.css'
 
-import { indexPage } from './interface/pages/index/index.js'
+import { indexPage } from 'Interface/pages/index/index.js'
+
+import { html, render } from 'MobiusUI'
+
+setTimeout(() => {
+  render(document.getElementById('mobius-app'), html`
+    <div>Welcome to use Mobius Template.</div>
+  `)
+}, 2000)
