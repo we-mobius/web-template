61b13780bf7363de4fbbf1ab813e8c3f74a662de
kongxiangyan <kcigaret@outlook.com>
2021-04-11 15:50:09 +0800

    chore: declare sideEffects

--- config/webpack.release.js
+++ config/webpack.release.js
@@ -35,7 +35,8 @@ export const getReleaseConfig = () => ({
           },
           'css-loader',
           'postcss-loader'
-        ]
+        ],
+        sideEffects: true
       },
       {
         oneOf: [...getReleaseLoaders()]
